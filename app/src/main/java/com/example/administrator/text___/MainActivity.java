de0ba67e09bedb821a48c66b74adee2279909c3d
lu19930520 <zl626520>
2016-10-19 15:55:55 +0800

    sss

--- app/src/main/java/com/example/administrator/text___/MainActivity.java
+++ app/src/main/java/com/example/administrator/text___/MainActivity.java
@@ -4,10 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * 陈创是个猪 哈哈哈哈哈
+     * @param savedInstanceState
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void get(){
+         String  question="陈创是猪么？";
+        String answer="of crouse";
+
     }
 }
